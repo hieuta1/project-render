01652be537e8f05df00005e152abe63ba1906c68
hieuta1 <shoppecuahieuta@gmail.com>
2025-12-12 18:42:54 +0700

    Serve frontend HTML and move API to /api

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -10,27 +9,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
-=======
-const express = require("express");
-const { Pool } = require("pg");
-
-const app = express();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
+// API backend
+app.get("/api", (req, res) => {
   res.json({
     message: "Backend Render CI/CD",
-    time: result.rows[0]
+    time: new Date()
   });
 });
 
-app.listen(process.env.PORT || 3000);
->>>>>>> c455b613e887ead1ca0feafb6ffce1a8dbd5446f
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
